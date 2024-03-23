5c7d59c01ebbfde7c2ce8ec8d96b9c4297a06e32
ALeks202020 <68061016+ALeks202020@users.noreply.github.com>
2024-03-23 08:53:05 +0200

    Delete .idea directory

--- src/main/java/net/education/kyivstar/config/PasswordEncryptor.java
+++ src/main/java/net/education/kyivstar/config/PasswordEncryptor.java
@@ -4,14 +4,15 @@ import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 
 public class PasswordEncryptor {
     private final String SECRET_WORD = "joke";
-    private final StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
 
     public String encrypt(String password) {
+        final StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
         encryptor.setPassword(SECRET_WORD);
         return encryptor.encrypt(password);
     }
 
     public String decrypt(String password) {
+        final StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
         encryptor.setPassword(SECRET_WORD);
         return encryptor.decrypt(password);
     }
